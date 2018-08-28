4d603c2bd9b22b38bc447951fef8849d7c362288
siznax <steve@siznax.net>
2018-08-27 18:09:56 -0700

    Removed OBSERVATIONS stub

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const OBSERVATIONS = [
-  {"id": 123, "photo": '123-photo', "taxon": '123-name', "user": '123-user', "date": '123-date'},
-  {"id": 456, "photo": '456-photo', "taxon": '456-name', "user": '456-user', "date": '456-date'},
-  {"id": 789, "photo": '789-photo', "taxon": '789-name', "user": '789-user', "date": '789-date'}
-];
-
 ReactDOM.render(
-  <App observations={OBSERVATIONS}/>,
+  <App observations={null} />,
   document.getElementById('root')
 );
 registerServiceWorker();
